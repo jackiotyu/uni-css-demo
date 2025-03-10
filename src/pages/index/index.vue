2d4ea019ddca192e0bfb7a254e77f2e505ba0721
jackhuang <2504448153@qq.com>
2025-03-10 13:52:19 +0800

    chore: 引入带css的组件

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <view>
+        <WithCss></WithCss>
+    </view>
+</template>
+
+<script>
+// 可以取消引入做对比
+import WithCss from './WithCss.vue';
+export default {
+    components: { WithCss },
+    data() {
+        return {
+            title: 'Hello',
+        };
+    },
+    onLoad() {},
+    methods: {},
+};
+</script>
